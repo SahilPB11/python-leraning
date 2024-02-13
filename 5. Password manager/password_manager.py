4457a4378e78f40023022fa001f81aa516f72db2
Sahil <garg07825@gmail.com>
2024-02-13 22:52:44 +0530

    password manager project completed

---
+++ 5. Password manager/password_manager.py
@@ -0,0 +1,58 @@
+#  pip install cryptography
+# Documentation:  https://cryptography.io/en/latest/fernet/
+
+from cryptography.fernet import Fernet
+
+'''
+this is we use to create a key ne time
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+
+write_key()
+'''
+
+# this is for get the key
+
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+key = load_key()
+fer = Fernet(key)
+
+
+def view():
+    with open('password.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("User: ", user, " Password: ",
+                  fer.decrypt(passw.encode()).decode())
+
+
+def add():
+    name = input("Account name: ")
+    pwd = input("Password: ")
+
+    with open('password.txt', 'a') as f:
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
+
+
+while True:
+    mode = input(
+        "Would u like to add a new password or view existing ones (view, add), press q to quit?: ").lower()
+    if mode == "q":
+        break
+    elif mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid mode.")
+        continue
